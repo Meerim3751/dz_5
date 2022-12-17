ca9bc6b113b0b813983a2c242c5fe32ca7511cd2
Igor <8luxfshdnlz1@mail.ru>
2022-12-17 14:03:50 +0600

    Посты, добавление постов

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Counter from './components/counter/counter'
 import SimpleForm from './components/simpleForm/simpleForm'
 import Toggles from './components/toggles/toggles'
+import PostList from './components/postList/postList'
+
 
 function App() {
 
   return (
     <div className="App">
-      <Toggles />
+      <PostList />
     </div>
   )
 }
